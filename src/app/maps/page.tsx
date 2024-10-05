db7ebeac508a4cf65b6f676acb869716f3051a53
John Doe~ <johndoe@example.com>
2024-10-05 11:24:06 +0800

    feat: rohman

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "../help/navbar";
 import { Component } from "./chart";
 import UserComponent from "../Home/[id]/hallo";
 import FeatureNew from "../Home/feature";
-
 interface IAppProps {}
 
 const Feature: React.FunctionComponent<IAppProps> = (props) => {
@@ -13,23 +12,29 @@ const Feature: React.FunctionComponent<IAppProps> = (props) => {
       <Navbar />
       <div className="md:px-[0px] h-[1800px] flex flex-nowrap items-center content-center justify-around bg-gray-00 border-black flex flex-col w-full x-20">
         <div className="border-b-[1px] border-black/80 w-full flex items-center justify-center">
-          {" "}
-          <p className="lg:text-2xl flex justify-center md:text-2xl  w-full items-center h-screen text-gray-900/80">
-            {" "}
-            <q>Explore the chart below <span className="text-orange-900/80 ml-1">to see how this feature works and the valuable benefits it offers!</span></q> 
-
-             </p> {" "}
-        </div> {" "} 
-        <div className="flex justify-center flex-nowrap "> 
-        <div className="w-90 flex justify-start flex-wrap">
-        <p className="py-8 text-xl w-full text-center text-900/80">   stock market live</p>
-        <div className="w-full flex justify-center">  <Component /> {" "} </div>
-         </div> </div>
+          <p className="lg:text-2xl flex justify-center md:text-2xl w-full items-center h-screen text-gray-900/80">
+            <q>
+              Explore the chart below 
+              <span className="text-orange-900/80 ml-1">
+                to see how this feature works and the valuable benefits it
+                offers!
+              </span>
+            </q>
+          </p>
         </div>
-
-   <FeatureNew /> 
-
-                   </div> 
+        <div className="flex justify-center flex-nowrap">
+          <div className="w-90 flex justify-start flex-wrap">
+            <p className="py-8 text-xl w-full text-center text-900/80">
+              stock market live
+            </p>
+            <div className="w-full flex justify-center">
+              <Component /> 
+            </div>
+          </div>
+        </div>
+      </div>
+      <FeatureNew />
+    </div>
   );
 };
 
